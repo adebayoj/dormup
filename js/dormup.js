5047359ec105322008cb11634a1899acfeec4fa9
Cecilia Testart <ctestart@30-9-44.wireless.csail.mit.edu>
2015-04-14 15:55:16 -0400

    adding js code

--- js/dormup.js
+++ js/dormup.js
@@ -1,1 +1,24 @@
 /* Insert JavaScript here */
+
+function startTime() {
+    var today=new Date();
+    var h=today.getHours();
+    var m=today.getMinutes();
+    m = checkTime(m);
+    document.getElementById('clock').innerHTML = h+":"+m;
+    var t = setTimeout(function(){startTime()},30000);
+}
+
+function checkTime(i) {
+    if (i<10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+
+$(document).ready(function(){
+
+    $("#btnPackage").click(function() {
+        window.location = "package.html";
+    });
+    
+});
