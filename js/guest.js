b06e5062c27e7c546232ec9d01b7995f3ae48bc6
mentzelos07 <mentzelosconstantinos@gmail.com>
2015-05-03 11:33:31 -0400

    fixing tabs

--- js/guest.js
+++ js/guest.js
@@ -1,7 +1,94 @@
 var selectedGuestId = [];
+var selectedGstId = -1;
 var selectedTab = "Residents";
 
-function getNextGuestId() {
+function unselectGuest(guestId) {
+    selectedGstId = -1;
+    removeRowHighlight(getGstRowId(guestId));
+}
+
+function selectGuest(guestId) {
+    selectedGstId = guestId;
+    highlightRow(getGstRowId(guestId));
+}
+
+function getGstId(rowId) {
+    return rowId.split("gst-")[1];
+}
+
+function getGstRowId(guestId) {
+    return "gst-" + guestId;
+}
+
+function isInGuestInfo(guestMapID, singleString){
+    var guestInfo=mapOfGuests[guestMapID];
+    var stringForSearch=singleString.toLowerCase();
+    if (guestInfo[0].toString().indexOf(stringForSearch)>=0 || guestInfo[2].toLowerCase().indexOf(stringForSearch)>=0){
+        return true
+    }
+    return false;
+}
+
+function setupGuestsList(searchStr) {
+    $("#firstColumn").html("Name");
+    $("#secondColumn").html("Status");
+    $("#residentList").empty();
+    if (searchStr === undefined || searchStr.length==0){
+        for (var r in mapOfGuests) {
+            if (!mapOfGuests.hasOwnProperty(r)) { // Ensure we're only using fields we added.
+                continue;
+            }
+            addGuestToList(r);
+        }
+    }
+    else{
+        var found=false;
+        var inputString=searchStr;
+        var inputArray=inputString.split(" ");
+        if (inputArray.length == 1){
+            for (var r in mapOfGuests) {
+                if (!mapOfGuests.hasOwnProperty(r)) { // Ensure we're only using fields we added.
+                    continue;
+                }
+                if (isInGuestInfo(r,inputArray[0])){
+                    addGuestToList(r);
+                    found=true;
+                }
+            }
+        }
+        else if (inputArray.length==2){
+            for (var r in mapOfGuests) {
+                if (!mapOfGuests.hasOwnProperty(r)) { // Ensure we're only using fields we added.
+                    continue;
+                }
+                if (isInGuestInfo(r,inputArray[0]) && isInGuestInfo(r,inputArray[1])){
+                    addGuestToList(r);
+                    found=true;
+                }
+            }
+        }
+        if (!found) {
+            $("#residentList").append(
+                '<div class="row">' +
+                '<div class="col-sm-12"><p><i>'+"\""+ inputString+"\""+ " not found."+'</i></p></div>' +
+                '</div>'
+            );
+        }
+    }
+    
+}
+
+function addGuestToList(guestId) {
+    var guests = mapOfGuests[guestId];
+    var rowId = getGstRowId(guestId);
+    $("#residentList").append(
+        '<div class="row" id="' + rowId + '">' +
+            '<div class="col-sm-8"><p>' + guests[0] + '</p></div>' +
+            '<div class="col-sm-4"><p>' + guests[2] + '</p></div>' +
+        '</div>');
+}
+
+function getNextGuestUniqueId() {
     var maxId = -1;
     for (var g in mapOfGuests) {
         if (!mapOfGuests.hasOwnProperty(g)) { // Ensure we're only using fields we added.
@@ -19,11 +106,11 @@ function setupRightSidebar(residentId) {
     clearGuestDetailsForm();
     selectedGuestId = [];
     $("#tableList").empty();
-    var arrayOfGuestIds = mapOfResidentsToGuests[residentId];
-    if(arrayOfGuestIds) {
-        for(var i = 0; i < arrayOfGuestIds.length; i++) {
-            guestId = arrayOfGuestIds[i];
-            addGuestDetailsToList(guestId, false);
+    var arrayOfGuestUniqueIds = mapOfResidentsToGuests[residentId];
+    if(arrayOfGuestUniqueIds) {
+        for(var i = 0; i < arrayOfGuestUniqueIds.length; i++) {
+            guestUniqueId = arrayOfGuestUniqueIds[i];
+            addGuestDetailsToList(guestUniqueId, false);
         }    
     }
     showOrHideListOptions(selectedGuestId.length);
@@ -39,36 +126,35 @@ function displayResidentProfile(residentId) {
     $("#room").val(resident[2]).prop('disabled', true);
 }
 
-function isSelected(guestId) {
+function isSelected(guestUniqueId) {
     for(var i = 0; i < selectedGuestId.length; i++) {
-        if(selectedGuestId[i] == guestId) {
+        if(selectedGuestId[i] == guestUniqueId) {
             return true;
         }
     }
     return false;
 }
 
-function deleteAllSelectedGuests(arrayOfGuestIds) {
+function deleteAllSelectedGuests(arrayOfGuestUniqueIds) {
     //TODO: Also remove guest from data model before removing from table.
-    for(var i = 0; i < arrayOfGuestIds.length; i++){
-        var guestId = arrayOfGuestIds[i];
+    for(var i = 0; i < arrayOfGuestUniqueIds.length; i++){
+        var guestId = arrayOfGuestUniqueIds[i];
         var rowId = "guest-" + guestId;
         deleteRowFromDisplay(rowId);
-        console.log(arrayOfGuestIds);
     }
     clearAllTableSelections();
     showOrHideListOptions(selectedGuestId.length);
 }
 
-function checkInAllSelectedGuests(arrayOfGuestIds) {
+function checkInAllSelectedGuests(arrayOfGuestUniqueIds) {
     var checkedInString = "Checked in";
-    for(var i = 0; i < arrayOfGuestIds.length; i++){
-        var guestId = arrayOfGuestIds[i];
+    for(var i = 0; i < arrayOfGuestUniqueIds.length; i++){
+        var guestId = arrayOfGuestUniqueIds[i];
         mapOfGuests[guestId][2] = checkedInString;
     }
 
-    for(var i = 0; i < arrayOfGuestIds.length; i++){
-        var guestId = arrayOfGuestIds[i];
+    for(var i = 0; i < arrayOfGuestUniqueIds.length; i++){
+        var guestId = arrayOfGuestUniqueIds[i];
         var rowId = "guest-" + guestId;
         var textSelector = "#" + rowId + " .status";
         $(textSelector).html(checkedInString);
@@ -77,21 +163,20 @@ function checkInAllSelectedGuests(arrayOfGuestIds) {
     }
 }
 
-function checkOutAllSelectedGuests(arrayOfGuestIds) {
+function checkOutAllSelectedGuests(arrayOfGuestUniqueIds) {
     var checkedOutString = "Checked out";
-    for(var i = 0; i < arrayOfGuestIds.length; i++){
-        var guestId = arrayOfGuestIds[i];
+    for(var i = 0; i < arrayOfGuestUniqueIds.length; i++){
+        var guestId = arrayOfGuestUniqueIds[i];
         mapOfGuests[guestId][2] = checkedOutString;
     }
-    for(var i = 0; i < arrayOfGuestIds.length; i++){
-        var guestId = arrayOfGuestIds[i];
+    for(var i = 0; i < arrayOfGuestUniqueIds.length; i++){
+        var guestId = arrayOfGuestUniqueIds[i];
         var rowId = "guest-" + guestId;
         var textSelector = "#" + rowId + " .status";
         $(textSelector).html(checkedOutString);
         temporarilyHighlightText(textSelector);
         deleteRowFromDisplay(rowId);
     }
-    console.log(arrayOfGuestIds.length);
     clearAllTableSelections();
     showOrHideListOptions(selectedGuestId.length);
 }
@@ -116,24 +201,24 @@ function saveNewGuestFromForm() {
     if(isEditing) {
         deleteAllSelectedGuests(selectedGuestId);
     }
-    var guestId = getGuestDetailsFromForm();
-    if(guestId == -1) {
+    var guestUniqueId = getGuestDetailsFromForm();
+    if(guestUniqueId == -1) {
         // Todo: implement more specific error message
         alert("please enter all required details in the right format.");
         return;
     }
     isEditing = false;
-    addGuestDetailsToList(guestId, true);
+    addGuestDetailsToList(guestUniqueId, true);
     clearGuestDetailsForm();
     showOrHideListOptions(selectedGuestId.length);
 }
 
-function addGuestDetailsToList(guestId, highlightRow){
-    var guest = mapOfGuests[guestId];
+function addGuestDetailsToList(guestUniqueId, highlightRow){
+    var guest = mapOfGuests[guestUniqueId];
     if(guest[2] == "Checked out") {
         return;
     }
-    var rowId = "guest-" + guestId;
+    var rowId = "guest-" + guestUniqueId;
     $("#tableList").prepend(
         '<div class="row" id="' + rowId + '" style="display:none">' +
             '<div class="col-sm-3 col-sm-offset-1"><p>' + guest[0] + '</p></div>' +
@@ -169,25 +254,25 @@ function getGuestDetailsFromForm() {
     if(!guestName || !checkIn || !status || !duration || !daysLeft) {
         return -1;
     }
-    var guestId = getNextGuestId();
+    var guestUniqueId = getNextGuestUniqueId();
     var guestDetails = [guestName, checkIn, status, duration, daysLeft, guestNote, guestId];
-    mapOfGuests[guestId] = guestDetails;
+    mapOfGuests[guestUniqueId] = guestDetails;
     var residentToGuestMapEntry = mapOfResidentsToGuests[selectedResidentId];
     if(residentToGuestMapEntry) {
         residentToGuestMapEntry.push(guestId);
     } else {
-        mapOfResidentsToGuests[selectedResidentId] = [guestId];
+        mapOfResidentsToGuests[selectedResidentId] = [guestUniqueId];
     }
-    return guestId;
+    return guestUniqueId;
 }
 
 function hideDaysLeft() {
     $("#daysLeftFormField").hide();
 }
 
-function showGuestDetailsForEditing(guestId) {
+function showGuestDetailsForEditing(guestUniqueId) {
     isEditing = true;
-    var guest = mapOfGuests[guestId];
+    var guest = mapOfGuests[guestUniqueId];
     $("#top-panel #guestName").val(guest[0]);
     $("#top-panel #checkIn").val(guest[1]);
     setDropdown(guest[2]);
@@ -198,7 +283,7 @@ function showGuestDetailsForEditing(guestId) {
     $("#saveNewGuest").show();
 }
 
-function getGuestId(rowId) {
+function getGuestUniqueId(rowId) {
     return rowId.split("guest-")[1];
 }
 
@@ -207,6 +292,24 @@ function getGuestRowId(residentId) {
 }
 
 $(document).ready(function(){
+
+    $("#myTab a").click(function(e){
+        e.preventDefault();
+        $(this).tab('show');
+    });
+
+    $("#residentsTab").click(function(e){
+        setupResidentList();
+        selectedTab = "Residents";
+        document.getElementById("searchInput").placeholder="Search by Name or Room Number";
+    });
+
+    $("#guestListTab").click(function(e){
+        setupGuestsList();
+        selectedTab = "Guests";
+        document.getElementById("searchInput").placeholder="Search by Name or Status";
+    });
+
     $("#table-menu #newFormBtn").click(function(){
         showFormForNewGuest();
         $("#top-panel").slideDown();      
@@ -250,16 +353,12 @@ $(document).ready(function(){
                 selectedGuestId[i] = selectedGuestId[selectedGuestId.length - 1];
                 selectedGuestId.pop();
                 removeRowHighlight(rowId);
-                console.log(rowId);
-                console.log("pop")
                 removedId = true;
             }
         }
         if(!removedId){
             selectedGuestId.push(guestId);    
             highlightRow(rowId);
-            console.log(rowId);
-                console.log("push")
         }
         showOrHideListOptions(selectedGuestId.length);
     });
